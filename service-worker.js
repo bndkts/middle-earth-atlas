06a71605ee07be624e7b8901615d118b139be61b
Benedikt Scheungraber <benedikt@MacBook-Air-von-Benedikt.local>
2026-09-13 22:01:48 +0200

    fix: refresh offline cache for Safari tint

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='middle-earth-atlas-';
-const CACHE=CACHE_PREFIX+'2026-09-expanded-help';
+const CACHE=CACHE_PREFIX+'2026-09-safari-tint';
 const CORE=[
   "/",
   "/offline.html",
