299036991b296f5ad0d3ef15a55a7f2aa3220ad1
Benedikt Scheungraber <benedikt@MacBook-Air-von-Benedikt.local>
2026-09-13 20:16:14 +0200

    feat: distinguish four woodland regions with native SVG silhouettes

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='middle-earth-atlas-';
-const CACHE=CACHE_PREFIX+'2026-09-map-camera';
+const CACHE=CACHE_PREFIX+'2026-09-regional-woods';
 const CORE=[
   "/",
   "/offline.html",
