49aa53fe380fe9d73c7b50f026dfb3e9568edb29
Benedikt Scheungraber <benedikt@MacBook-Air-von-Benedikt.local>
2026-09-13 22:37:31 +0200

    fix: smooth river junctions and mouth transitions

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='middle-earth-atlas-';
-const CACHE=CACHE_PREFIX+'2026-09-mobile-map';
+const CACHE=CACHE_PREFIX+'2026-09-river-transitions';
 const CORE=[
   "/",
   "/offline.html",
